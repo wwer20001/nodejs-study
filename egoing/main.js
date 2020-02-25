8a4d47f257e9d4617b01dc5255c942bc657252c9
wwer20001 <wwer20001@naver.com>
2020-02-25 18:17:20 +0900

    20200225
    숫자, 문자열, 변수

--- egoing/main.js
+++ egoing/main.js
@@ -12,4 +12,4 @@ var app = http.createServer(function(request, response){
     console.log(__dirname + url);
     response.end(fs.readFileSync(__dirname + url));
 });
-app.listen(3000);
+app.listen(3000); 
