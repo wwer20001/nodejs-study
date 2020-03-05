548c3244efec42f0195258d9d8b30ac60e02040c
wwer20001 <wwer20001@naver.com>
2020-03-05 13:39:45 +0900

    20200305-생활코딩-19.2강

--- egoing/main.js
+++ egoing/main.js
@@ -6,32 +6,60 @@ var app = http.createServer(function(request, response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
 
     if(pathname === '/'){
-      fs.readFile(`data/${queryData.id}`, 'utf8', (err, desc) => {
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JaveScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${desc}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
-      });
+      if(queryData.id === undefined){
+        fs.readFile(`data/${queryData.id}`, 'utf8', (err, desc) => {
+          var title = 'Welcome';
+          var desc = 'Hello, Node.js';
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JaveScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${desc}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      }else{
+        fs.readFile(`data/${queryData.id}`, 'utf8', (err, desc) => {
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JaveScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${desc}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      }
     }else{
       response.writeHead(404);
       response.end('Not found');
