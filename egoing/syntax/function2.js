96479969410929c5a365ad8e53fadb7725e97d2d
wwer20001 <wwer20001@naver.com>
2020-03-09 19:19:11 +0900

    20200309-생활코딩-25.3강

--- egoing/syntax/function2.js
+++ egoing/syntax/function2.js
@@ -2,7 +2,9 @@ console.log(Math.round(1.6));   // 2
 console.log(Math.round(1.4));   // 1
 
 function sum(n1, n2){   // parameter
-    console.log(n1 + n2);
+    console.log('start');
+    return n1 + n2;
+    console.log('end');
 }
 
-sum(2, 4);  // argument
+console.log(sum(2, 4));  // argument
