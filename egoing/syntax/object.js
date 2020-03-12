b8b4a9973c7b2a3b5df9fff559259d0af52a18fd
wwer20001 <wwer20001@naver.com>
2020-03-12 21:28:17 +0900

    20200312-생활코딩-40강

--- egoing/syntax/object.js
+++ egoing/syntax/object.js
@@ -1,8 +1,19 @@
 var members = ['egoing', 'k8805', 'hoya'];
 console.log(members[1]);    // k8805
+var i = 0;
+while(i < members.length) {
+    console.log(i + " / " + members[i]);
+    i+= 1;
+}
+
 var roles = { 
-    'programmer':'egoing', 
+    'programmer':'egoing', // key : value
     'designer':'k8805',
     'manager':'hoya' 
 };
 console.log(roles.designer);    // k8805
+console.log(roles['designer']);    // k8805
+
+for(var name in roles){
+    console.log('key => ', name, ' value => ', roles[name]);
+}
