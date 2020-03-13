797e8d721bc21dab5a5143d08cca1d78a34bbd64
wwer20001 <wwer20001@naver.com>
2020-03-13 21:01:35 +0900

    20200313-생활코딩-42강

---
+++ egoing/syntax/object3.js
@@ -0,0 +1,18 @@
+// var v1 = 'v1';
+// // 100000 code
+// v1 = 'egoing';
+// var v2 = 'v2';
+
+var q = {
+    v1:'v1',
+    v2:'v2', 
+    f1:function (){
+        console.log(this.v1);
+    },
+    f2:function (){
+        console.log(this.v2);
+    }
+};
+
+q.f1();
+q.f2();
